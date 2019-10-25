d9c2bc5bccbe0bfa87579fdf3722f4a4368d16a6
Andrzej Chemerinskiy <andrzej.chemerinskiy@blue-veery.com>
2019-10-25 20:18:49 +0200

    Updates

---
+++ precache-manifest.36572ff1154b353eb45fb92e6d64a3f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9e83df1ca8354641a7565f0d28534c",
+    "url": "/list-hotels/index.html"
+  },
+  {
+    "revision": "c99e62e81bc82628663d",
+    "url": "/list-hotels/static/css/main.52352d61.chunk.css"
+  },
+  {
+    "revision": "1fcad0846f661cfb8f6d",
+    "url": "/list-hotels/static/js/2.25f3f88d.chunk.js"
+  },
+  {
+    "revision": "c99e62e81bc82628663d",
+    "url": "/list-hotels/static/js/main.2beb6fa5.chunk.js"
+  },
+  {
+    "revision": "38ab686389b3ff08dcd8",
+    "url": "/list-hotels/static/js/runtime-main.016c5b0c.js"
+  },
+  {
+    "revision": "09b773495df08c18c7064d7721770408",
+    "url": "/list-hotels/static/media/placeholder.09b77349.jpg"
+  }
+]);
